40a2c8c5fb6ee9e54462a55290c8c557055418ef
jlopez43-hub <jlopez43@mail.csuchico.edu>
2024-03-05 15:26:25 -0800

    made scripts for player and camera movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,26 +8,35 @@ public class InputManager : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
 
-
+    private PlayerMotor motor;
+    private PlayerLook look;
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+        look = GetComponent<PlayerLook>();
     }
   
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //the the playermotor to move 
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
-    private void OnEnabled()
+    private void OnEnable()
     {
         onFoot.Enable();
     }
-    private void OnDisabled()
+    private void OnDisable()
     {
         onFoot.Disable();
     }
