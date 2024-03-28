f7d6b6bb700490cabdde94fa0eaab51947bc1db4
jlopez43-hub <jlopez43@mail.csuchico.edu>
2024-03-27 20:38:46 -0700

    Able to have the player react the blast but camera and movement is not moving

--- Assets/Scripts/RigidBodyMovement.cs
+++ Assets/Scripts/RigidBodyMovement.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/*
+ * Judith Lopez
+ * 03/26/2024
+ * propelling the player when in contact with explosion
+ */
 public class RigidBodyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 3f;
+    private PlayerInputController playerActions;
+    private PlayerInputController.MoveActions move;
+
+    public float jumpHeight = 10f;
+
+    //camera variables
+    private Vector2 PlayerMouseInput;
+    private float xRot;
+    private float sensitivity;
+    public Transform PlayerCamera;
+    public Rigidbody PlayerBody;
+
+    private void Awake()
     {
-        
+        playerActions = new PlayerInputController();
+        move = playerActions.Move;
+
+        playerActions.Enable();
+        playerActions.Disable();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //update called once per frame
+    private void FixedUpdate()
     {
-        
+        Vector2 moveVec = playerActions.Move.Move.ReadValue<Vector2>();
+        GetComponent<Rigidbody>().AddForce(new Vector3(moveVec.x, 0, moveVec.y) * speed, ForceMode.Force);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump:" + context.phase);
+        if (context.performed)
+        {
+            Debug.Log("Real Jump");
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+
+
+
+    public void MovePlayerCamera(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>());
+
+        xRot -= context.ReadValue<Vector2>().y * sensitivity;
+
+        //transform.Rotate(Of, context.ReadValue<Vector2>().x * sensitivity, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(0, xRot, 0f);
+        //PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 moveVec = context.ReadValue<Vector2>();
+        GetComponent<Rigidbody>().AddForce(new Vector3(moveVec.x, 0, moveVec.y) * 5f, ForceMode.Force);
     }
 }
