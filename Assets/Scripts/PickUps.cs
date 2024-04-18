b432dab81a2dab29a150fd87c742fab2c760a9a4
jlopez43-hub <jlopez43@mail.csuchico.edu>
2024-04-18 08:10:31 -0700

    created a health pickup prefab and script

---
+++ Assets/Scripts/PickUps.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Author [Judith Lopez]
+ * Date: 04/17/2024
+ * Creating health pickups for when the player is low on health
+ */
+
+public enum PickupTypes { HEALTH } // can add more values
+
+public class PickUps : MonoBehaviour
+{
+    public PickupTypes pickup;
+
+    public float myValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
